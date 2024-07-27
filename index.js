4f0a26fdfd4fe12e1f62dafbf8a603f35b5acd35
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-07-27 13:20:00 +0200

    feat: add default export for WebComponent

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import WebComponent from "./WebComponent.js"
 import Component from "./Component.js"
 import Router from "./Router.js"
 
-export { WebComponent, Component, Router }
+export default WebComponent
+
+export {
+  Component,
+  Router,
+  WebComponent // This is the default export. It is kept here for backwards compatibility.
+               // @Todo: Remove it in the next major version.
+}
